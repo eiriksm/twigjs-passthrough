2017a8fa207e1e19f38e9296cf93607181fc80b7
Eirik S. Morland <eirik@morland.no>
2016-04-03 12:59:20 +0200

    Add tests and coverage.

--- test/test.js
+++ test/test.js
@@ -1,1 +1,17 @@
-console.log('todo');
+'use strict';
+/* global describe, it */
+require('should');
+
+describe('Module functionality', function() {
+  it('Should export a function', function() {
+    require('..').should.be.instanceOf(Function);
+  });
+  it('Should return the same value when doing filtering', function() {
+    var value = 'test123';
+    require('..')({
+      extendFilter: function(t, c) {
+        c(value).should.equal(value);
+      }
+    });
+  })
+});
